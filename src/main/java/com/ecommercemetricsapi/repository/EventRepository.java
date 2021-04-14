ea7e76d37d88b7d9e1d4b61b968c50c495030e5a
joseflauzino <jose.wilson.vf@gmail.com>
2021-04-14 11:57:49 -0300

    Improving searches

--- src/main/java/com/ecommercemetricsapi/repository/EventRepository.java
+++ src/main/java/com/ecommercemetricsapi/repository/EventRepository.java
@@ -1,9 +1,14 @@
 package com.ecommercemetricsapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ecommercemetricsapi.models.Company;
 import com.ecommercemetricsapi.models.Event;
 
 public interface EventRepository extends JpaRepository<Event,Long>{
 	Event findById(long id);
+	List<Event> findByType(String type);
+	List<Event> findByCompany(Company company);
 }
--- src/main/java/com/ecommercemetricsapi/resources/MetricsResource.java
+++ src/main/java/com/ecommercemetricsapi/resources/MetricsResource.java
@@ -4,13 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommercemetricsapi.models.Company;
@@ -42,20 +38,20 @@ public class MetricsResource {
 	}
 	
 	@GetMapping(value="/company/{id}/metric/conversion-rate")
-	@ApiOperation(value="Returns the conversion rate of a given company")
+	@ApiOperation(value="Returns the conversion rate of a given company | (sales number ÷ visit number) x 100")
 	public Metric getConversionRate(@PathVariable(value="id") long id){
 		// (sales number ÷ visit number) x 100
 		Company company = companyRepository.findById(id);
+		List<Event> companyEvents = eventRepository.findByCompany(company);
 		List<Event> sales = new ArrayList<>();
-		for (Event e : eventRepository.findAll()) {
-			System.out.println(e);
-			if(e.getCompany().equals(company) && "sale".equals(e.getType())) {
+		for (Event e : companyEvents) {
+			if("sale".equals(e.getType())) {
 				sales.add(e);
 			}
 		}
 		List<Event> visits = new ArrayList<>();
-		for (Event e : eventRepository.findAll()) {
-			if(e.getCompany().equals(company) && "visit".equals(e.getType())) {
+		for (Event e : companyEvents) {
+			if("visit".equals(e.getType())) {
 				visits.add(e);
 			}
 		}
@@ -64,19 +60,20 @@ public class MetricsResource {
 	}
 	
 	@GetMapping(value="/company/{id}/metric/cart-abandonment-rate")
-	@ApiOperation(value="Returns the cart abandonment rate of a given company")
+	@ApiOperation(value="Returns the cart abandonment rate of a given company | (carts with added products ÷ sales) x 100")
 	public Metric getCartAbandonmentRate(@PathVariable(value="id") long id){
 		// (number of carts with added products ÷ sales number ) x 100
 		Company company = companyRepository.findById(id);
+		List<Event> companyEvents = eventRepository.findByCompany(company);
 		List<Event> carts = new ArrayList<>();
-		for (Event e : eventRepository.findAll()) {
-			if(e.getCompany().equals(company) && "cart".equals(e.getType())) {
+		for (Event e : companyEvents) {
+			if("cart".equals(e.getType())) {
 				carts.add(e);
 			}
 		}
 		List<Event> sales = new ArrayList<>();
-		for (Event e : eventRepository.findAll()) {
-			if(e.getCompany().equals(company) && "sale".equals(e.getType())) {
+		for (Event e : companyEvents) {
+			if("sale".equals(e.getType())) {
 				sales.add(e);
 			}
 		}
@@ -85,20 +82,21 @@ public class MetricsResource {
 	}
 	
 	@GetMapping(value="/company/{id}/metric/approved-order-rate")
-	@ApiOperation(value="Returns the approved order rate of a given company")
+	@ApiOperation(value="Returns the approved order rate of a given company | (paid orders ÷ orders) x 100")
 	public Metric getApprovedOrderRate(@PathVariable(value="id") long id){
 		// (number of paid orders ÷ number of orders) x 100
 		// sale == paid-order
 		Company company = companyRepository.findById(id);
+		List<Event> companyEvents = eventRepository.findByCompany(company);
 		List<Event> paidOrders = new ArrayList<>();
-		for (Event e : eventRepository.findAll()) {
-			if(e.getCompany().equals(company) && "sale".equals(e.getType())) {
+		for (Event e : companyEvents) {
+			if("sale".equals(e.getType())) {
 				paidOrders.add(e);
 			}
 		}
 		List<Event> orders = new ArrayList<>();
-		for (Event e : eventRepository.findAll()) {
-			if(e.getCompany().equals(company) && "order".equals(e.getType())) {
+		for (Event e : companyEvents) {
+			if("order".equals(e.getType())) {
 				orders.add(e);
 			}
 		}
